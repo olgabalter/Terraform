85ca70a59e07bf868a900483f8e74b1e8b304e62
olgabalter <40796008+olgabalter@users.noreply.github.com>
2025-01-02 12:12:09 +0200

    Create variable.tf

---
+++ Chubb/projectEntitlement/variable.tf
@@ -0,0 +1,20 @@
+# Input variable: url
+variable "url" {
+  description = "The URL of the vRealize Automation environment either vRA 8.x or vRA Cloud"
+  type = string
+  default = "https://ca-vra.terasky.local"
+}
+
+# Input variable: project_name
+variable "project_name" {
+  description = "Created vRA project name"
+  type = string
+  default = ""
+}
+
+# Input variable: content_source
+variable "content_source" {
+  description = "Content source name"
+  type = string
+  default = ""
+}
