12e4403d249b1dd1c6e27f95859a89067005edd8
olgabalter <40796008+olgabalter@users.noreply.github.com>
2025-01-02 12:55:36 +0200

    Update variable.tf

--- Chubb/projectEntitlement/variable.tf
+++ Chubb/projectEntitlement/variable.tf
@@ -25,3 +25,17 @@ variable "content_source" {
   type = string
   default = ""
 }
+
+# Input variable: policy_name
+variable "policy_name" {
+  description = "Content sharing policy name"
+  type = string
+  default = ""
+}
+
+# Input variable: poilcy_description
+variable "poilcy_description" {
+  description = "Content sharing policy description"
+  type = string
+  default = ""
+}
